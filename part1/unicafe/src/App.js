881d0ff0238989c83c1acf3214c8aef37bc42b58
Amalia <amalia@pop-os.localdomain>
2022-02-09 18:55:21 +0200

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,11 +4,7 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({ good, neutral, bad }) => {
 
   const all = () => (
     good + neutral + bad
@@ -20,7 +16,25 @@ const App = () => {
 
   const positive = () => (
     good / all()
-  ) 
+  )
+  
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all()}</p>
+      <p>Average: {average()}</p>
+      <p>Positive: {positive()} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
@@ -29,12 +43,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='NEUTRAL' />
       <Button handleClick={() => setBad(bad + 1)} text='BAD' />
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all()}</p>
-      <p>Average: {average()}</p>
-      <p>Positive: {positive()} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
