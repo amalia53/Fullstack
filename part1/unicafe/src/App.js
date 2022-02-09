5ad344268886c853edbffafe9301dddcd4c5368d
Amalia <amalia@pop-os.localdomain>
2022-02-09 18:47:01 +0200

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,6 +10,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = () => (
+    good + neutral + bad
+  )
+
+  const average = () => (
+    (good - bad) / all()
+  )
+
+  const positive = () => (
+    good / all()
+  ) 
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -20,6 +32,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all()}</p>
+      <p>Average: {average()}</p>
+      <p>Positive: {positive()} %</p>
     </div>
   )
 }
