46d7333234549a8bee4cf10867a01136fa2516b4
Amalia <amalia.ahola@gmail.com>
2022-02-12 17:01:39 +0200

    2.4-2.5

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -2,10 +2,42 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = () => {
+  const courses = [
+    Course('Half Stack application development', 1, [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+      {
+        name: 'Communicating with server',
+        exercises: 12
+      }
+    ]),
+    Course('Node.js', 2,[
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ])
+  ]
   return (
-    <div>
-      <Course></Course>
-    </div>
+    <>
+      {courses.map(course => <div key={course.id}>{course.text}</div> )}
+    </>
   )
 }
 
--- part1/kurssitiedot/src/components/Course.js
+++ part1/kurssitiedot/src/components/Course.js
@@ -2,34 +2,21 @@ import React from 'react'
 import Content from './Content'
 import Header from './Header'
 
-const Course = () => {
+const Course = (name, id, parts) => {
 	const course = {
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7
-			},
-			{
-				name: 'State of a component',
-				exercises: 14
-			},
-			{
-				name: 'Communicating with server',
-				exercises: 12
-			}
-		]
+		name: name,
+		parts: parts
 	}
 	return (
-		<>
-			<Header course={course} />
-			<Content course={course} />
-			<b>Total of <Total course={course} /> exercises</b>
-		</>
+		{
+			id: id,
+			text: 
+			<>
+				<Header course={course} />
+				<Content course={course} />
+				<b>Total of <Total course={course} /> exercises</b>
+			</>
+		}
 	)
 }
 
