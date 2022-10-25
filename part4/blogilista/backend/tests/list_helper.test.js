ccde7918506ff353ec10c42f0e54fb5bd1aaa9d1
Amalia <amalia@pop-os.localdomain>
2022-10-25 17:31:19 +0200

    4.4

--- part4/blogilista/backend/tests/list_helper.test.js
+++ part4/blogilista/backend/tests/list_helper.test.js
@@ -1,9 +1,54 @@
 const listHelper = require('../utils/list_helper')
 
-test("dummy returns one", () => {
-  const blogs = []
+const empty = []
 
-  const result = listHelper.dummy(blogs)
+const blogs = [
+  {
+    title: "title",
+    author: "author",
+    url: "url",
+    likes: 1,
+  },
+  {
+    title: "title2",
+    author: "author2",
+    url: "url2",
+    likes: 2,
+  },
+  {
+    title: "title3",
+    author: "author3",
+    url: "url3",
+    likes: 3,
+  }
+]
+
+test("dummy returns one", () => {
+  const result = listHelper.dummy(empty)
 
   expect(result).toBe(1)
+})
+
+describe("total likes", () => {
+
+  test("of an empty list returns 0", () => {
+    const result = listHelper.totalLikes(empty)
+
+    expect(result).toBe(0)
+  })
+  
+  test("of list of three blogs returns total of likes of all blogs", () => {
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(6)
+  })
+
+  test(" of list of one blog returns likes of the blog", () => {
+    blogs.pop()
+    blogs.pop()
+
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(blogs[0].likes)
+  })
 })
