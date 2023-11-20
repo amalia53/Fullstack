5f0904a899737af44d6f8c0d51086ee764889b25
Amalia <amalia@pop-os.localdomain>
2023-11-20 15:11:57 +0100

    4.8.)

--- part4/blogilista/backend/tests/blog_api.test.js
+++ part4/blogilista/backend/tests/blog_api.test.js
@@ -3,12 +3,42 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('blogs are return as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+const initialBlogs = [
+  {
+    "title": "Test title 1",
+    "author": "Test author 1",
+    "url": "Test url 1",
+    "likes": 1
+  },
+  {
+    "title": "Test title 2",
+    "author": "Test author 2",
+    "url": "Test url 2",
+    "likes": 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length)
   })
 
 afterAll(async () => {
