04fbde37e835d5cd7ab400cc2201f84b4bece655
Amalia <amalia@pop-os.localdomain>
2023-11-22 13:28:09 +0100

    4.16

--- part4/blogilista/backend/controllers/users.js
+++ part4/blogilista/backend/controllers/users.js
@@ -5,18 +5,35 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+    if (password === undefined || password.length < 3) {
+        const errormessage =
+            password === undefined
+                ? 'Password is required'
+                : 'Password must be at least 3 characters long'
+        response.status(400).json(errormessage)
+    } else {
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    })
+        const user = new User({
+            username,
+            name,
+            passwordHash,
+        })
 
-    const savedUser = await user.save()
-
-    response.status(201).json(savedUser)
+        try {
+            const users = await User.find({})
+            const usernames = users.map(user => user.username)
+            if (usernames.includes(username)) {
+                response.status(400).json('Username is already in use')
+            } else {
+                const savedUser = await user.save()
+                response.status(201).json(savedUser)
+            }
+        } catch (exception) {
+            response.status(400).json(exception.errors.username.message)
+        }
+    }
 })
 
 usersRouter.get('/', async (request, response) => {
--- part4/blogilista/backend/models/user.js
+++ part4/blogilista/backend/models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        minLength: [3, 'Username must be at least 3 characters long']
+      },
     name: String,
     passwordHash: String,
 })
