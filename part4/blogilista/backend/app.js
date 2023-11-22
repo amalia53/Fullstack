13edb9a6d85ddfa95d711e05aa609a8b9f9cc6dd
Amalia <amalia@pop-os.localdomain>
2023-11-22 11:57:45 +0100

    4.15

--- part4/blogilista/backend/app.js
+++ part4/blogilista/backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 app = express()
 const cors = require('cors')
 const blogsRouter = require("./controllers/blogs")
+const usersRouter = require('./controllers/users')
 const mongoose = require("mongoose")
 
 mongoose.set('strictQuery', false)
@@ -14,5 +15,6 @@ app.use(express.json())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
