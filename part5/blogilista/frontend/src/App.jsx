04d016ea131ab1c282c4e553b19f0fa9489aa7bd
Amalia <amalia@pop-os.localdomain>
2023-11-29 10:55:26 +0100

    5.1.-5.3.

--- part5/blogilista/frontend/src/App.jsx
+++ part5/blogilista/frontend/src/App.jsx
@@ -5,11 +5,24 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const [username, setUsername] = useState('')
@@ -23,6 +36,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,6 +51,28 @@ const App = () => {
       }, 5000)
     }
     console.log('logging in with', username)
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    window.localStorage.clear()
+    blogService.setToken(null)
+    setUser(null)
+  }
+
+  const handleCreation = async (event) => {
+    event.preventDefault()
+    const newBlog = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+    const added = await blogService.create(newBlog)
+    setBlogs(blogs.concat(added))
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
   }
 
   if (user === null) {
@@ -65,12 +105,29 @@ const App = () => {
   } else {
     return (
       <div>
-      <h2>Blogs</h2>
-      <div>{user.name} is logged in</div>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+        <h2>Blogs</h2>
+        <div>{user.name} is logged in</div>
+        <button onClick={handleLogout}>LOGOUT</button>
+        <h3>Create new</h3>
+        <form onSubmit={handleCreation}>
+          <div>
+            Title: <input type="text" value={newTitle}
+              onChange={({ target }) => setNewTitle(target.value)} />
+          </div>
+          <div>
+            Author: <input type="text" value={newAuthor}
+              onChange={({ target }) => setNewAuthor(target.value)} />
+          </div>
+          <div>
+            Url: <input type="text" value={newUrl}
+              onChange={({ target }) => setNewUrl(target.value)} />
+          </div>
+          <button type="submit">Create</button>
+        </form>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
     )
   }
 
