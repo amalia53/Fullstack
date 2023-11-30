cf6c01fe8cb038ea07b25a4db5e0d68a79c6f1ad
Amalia <amalia@pop-os.localdomain>
2023-11-30 15:01:51 +0100

    5.7

--- part5/blogilista/frontend/src/components/Blog.jsx
+++ part5/blogilista/frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewMore, setViewMore] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const like = () => {
+    console.log('Liked!')
+  }
+
+  const showMore = () => {
+    return (
+      <div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button onClick={() => like()}>like</button>
+        </div>
+        <div>
+          Added by {blog.user.name}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author} <button onClick={() => setViewMore(!viewMore)}>{viewMore ? 'hide' : 'view'}</button>
+      {viewMore && showMore()}
+    </div>
+  )
+}
 
 export default Blog
