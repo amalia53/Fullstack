1f60070d501cfdcdf71aac67c8e29beb7470bbbd
Amalia <amalia@pop-os.localdomain>
2023-12-06 15:04:17 +0100

    5.18

--- part5/blogilista/frontend/cypress/e2e/blogilista.cy.js
+++ part5/blogilista/frontend/cypress/e2e/blogilista.cy.js
@@ -3,37 +3,34 @@ describe('Blogilista', function () {
   beforeEach(function () {
     cy.visit('http://localhost:5173')
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test Person',
+      username: 'testperson',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
   })
 
   it('Login form is shown', function () {
     cy.contains('Blogs')
     cy.contains('Log in')
+    cy.contains('LOGIN').click()
+  })
+
+
+  describe('Login', function () {
+
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('testperson')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+    })
+
+    it.only('fails with wrong credentials', function () {
+      cy.get('#username').type('testperson')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+      cy.get('.error').contains('Wrong credentials')
+    })
   })
 })
-
-// it('Login form is shown', function() {
-//   cy.visit('http://localhost:5173')
-//   cy.contains('Log in')
-//   cy.contains('LOGIN').click()
-// })
-
-// it('User can log in', function() {
-//   const user = {
-//     name: 'Test Person',
-//     username: 'testperson',
-//     password: 'testpassword'
-
-//   }
-//   cy.request('POST', 'http://localhost:3003/api/users/', user)
-// })
-
-// describe('when logged in', function () {
-//   beforeEach(function () {
-//     cy.visit('http://localhost:5173')
-//     cy.contains('LOGIN').click()
-//     cy.get('#username').type('testperson')
-//     cy.get('#password').type('testpassword')
-//     cy.get('#login-button').click()
-//   })
-// })
-// })
