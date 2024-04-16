981c4f1557a122bcef8ba6964e3ffc835984be6c
Amalia <amalia@pop-os.localdomain>
2024-04-16 15:34:08 +0300

    6.15

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -6,4 +6,12 @@ const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
-export default { getAll }
+
+const createNew = async (content) => {
+  const object = { content, votes: 0 }
+  const res = await axios.post(baseUrl, object)
+  return res.data
+}
+
+
+export default { getAll, createNew }
