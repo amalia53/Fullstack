a501cd56f4ed819eadf4b134ea5be42ffdf17a00
Amalia Ahola <amalia.ahola@gmail.com>
2024-07-10 12:14:38 +0300

    9.14

---
+++ part9/vite-app/src/components/Content.tsx
@@ -0,0 +1,21 @@
+interface ContentProps {
+  courseParts: { name: string; exerciseCount: number }[];
+}
+
+const Contents = (props: ContentProps) => {
+  return (
+    <div>
+      <p>
+        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
+      </p>
+    </div>
+  );
+};
+
+export default Contents;
